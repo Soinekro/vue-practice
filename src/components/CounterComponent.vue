43d4eccf0cc2f95c13b12cdb372864e3ecac74db
Soinekro <echumaceroj@ucvvirtual.edu.pe>
2024-11-19 12:00:49 -0500

    installacion de vue-router@v4

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="counter">
+        <h1>Counter</h1>
+        <p>{{ count }}</p>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CounterComponent',
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.count++
+        },
+        decrement() {
+            this.count--
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+h1{
+    font-size: 2em;
+    color: red;
+}
+.counter {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
+}
+</style>
